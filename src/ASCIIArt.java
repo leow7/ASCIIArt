ef136c6be92950fbea3a10640ee44553ca9455ad
leow7 <100174028@lubbockisd.net>
2023-08-31 08:57:34 -0500

    This is my ASCII art.

---
+++ src/ASCIIArt.java
@@ -0,0 +1,15 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println("\\****__              ____");
+        System.out.println("   |    *****\\_      --/ *\\-__");
+        System.out.println("   /_          (_    ./ ,/----'");
+        System.out.println("     \\__         (_./  /");
+        System.out.println("        \\__           \\___----^__");
+        System.out.println("         _/   _                  \\");
+        System.out.println("  |    _/  __/ )\\\"\\ _____         *\\");
+        System.out.println("  |\\__/   /    ^ ^       \\____      )");
+        System.out.println("   \\___--\"                    \\_____ )");
+        System.out.println("                                    \"");
+        System.out.println("Art by Ironwing");
+    }
+}
